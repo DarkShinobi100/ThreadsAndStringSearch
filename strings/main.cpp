18334fdbf92c072ed5c74ba88e58186c653a0c96
edwyn mckie <37442282+DarkShinobi100@users.noreply.github.com>
2020-03-25 18:34:20 +0000

    timing code now added

--- strings/main.cpp
+++ strings/main.cpp
@@ -38,6 +38,9 @@ typedef std::chrono::steady_clock the_clock;
 //my file to save values to
 ofstream my_file("Timings.csv");
 #define d 256 //number of characters in the english alphabet
+const int BOOKLIMIT = 6;
+const int PATTERNCOUNT = 20;
+
 vector<Position> BooyerMooreResults;
 vector<Position> RabinKarpeResults;
 mutex save_mutex;
@@ -149,7 +152,6 @@ Position find_bm(const string& pat, const string& text) {
 /* skip ahead by the length of the string if we do not match.but keep looping through the whole file */
 void find_bm_multiple(const string& pat, const string& text) 
 {
-	cout << "BoyerMoore:" << endl;
 	Position pat_len = pat.size();
 	Position text_len = text.size();
 	vector<Position> Results;
@@ -196,14 +198,12 @@ void find_bm_multiple(const string& pat, const string& text)
 		}
 	}
 	BooyerMoore_ready = true;
-	cout << pat << " was found: " << NumberofMatches << " time(s)" << endl;
+//	cout << pat << " was found: " << NumberofMatches << " time(s)" << endl;
 }
 
 /** read the patern and calculate a value for it, read the next cunk of the file we are searching and compare the two. slide along by 1 and calculate again */
 void Rabin_Karp(const string& pat, const string& text) 
 {
-	cout << "Rabin Karp:"<< endl;
-
 	int PatternLength = pat.size();
 	int TextLength = text.size();
 	int PrimeNumber = 2;
@@ -255,7 +255,7 @@ void Rabin_Karp(const string& pat, const string& text)
 		}
 	}
 	RabinKarp_ready = true;
-	cout << pat << " was found: " << NumberofMatches << " time(s)" << endl;
+//	cout << pat << " was found: " << NumberofMatches << " time(s)" << endl;
 }
 
 void finished_book(int volume)
@@ -272,159 +272,53 @@ void old_string_Search()
 	//STRING SEARCH DEFAULT CODE
 
 	string text;//declare text as a string
-	float time_taken[2];
 	string FileName = "";
 
-	string pat[100]; //pat = pattern we are looking for
+	string pat[PATTERNCOUNT];
 	pat[0] = "244";
-	pat[1] = "244";
-	pat[2] = "244";
-	pat[3] = "244";
-	pat[4] = "244";
-	pat[5] = "244";
-	pat[6] = "244";
-	pat[7] = "244";
-	pat[8] = "244";
-	pat[9] = "244";
-	// repeated these 10 values to get a consistent average for each one
-	pat[10] = "Tohka";
+	pat[1] = "Tohka";
+	pat[2] = "Origami";
+	pat[3] = "Yoshino";
+	pat[4] = "Shido";
+	pat[5] = "Spirit";
+	pat[6] = "DEM";
+	pat[7] = "Angel";
+	pat[8] = "Battle";
+	pat[9] = "Blade";
+	pat[10] = "244";
 	pat[11] = "Tohka";
-	pat[12] = "Tohka";
-	pat[13] = "Tohka";
-	pat[14] = "Tohka";
-	pat[15] = "Tohka";
-	pat[16] = "Tohka";
-	pat[17] = "Tohka";
-	pat[18] = "Tohka";
-	pat[19] = "Tohka";
-	pat[20] = "Origami";
-	pat[21] = "Origami";
-	pat[22] = "Origami";
-	pat[23] = "Origami";
-	pat[24] = "Origami";
-	pat[25] = "Origami";
-	pat[26] = "Origami";
-	pat[27] = "Origami";
-	pat[28] = "Origami";
-	pat[29] = "Origami";
-	pat[30] = "Yoshino";
-	pat[31] = "Yoshino";
-	pat[32] = "Yoshino";
-	pat[33] = "Yoshino";
-	pat[34] = "Yoshino";
-	pat[35] = "Yoshino";
-	pat[36] = "Yoshino";
-	pat[37] = "Yoshino";
-	pat[38] = "Yoshino";
-	pat[39] = "Yoshino";
-	pat[40] = "Shido";
-	pat[41] = "Shido";
-	pat[42] = "Shido";
-	pat[43] = "Shido";
-	pat[44] = "Shido";
-	pat[45] = "Shido";
-	pat[46] = "Shido";
-	pat[47] = "Shido";
-	pat[48] = "Shido";
-	pat[49] = "Shido";
-	pat[50] = "Spirit";
-	pat[51] = "Spirit";
-	pat[52] = "Spirit";
-	pat[53] = "Spirit";
-	pat[54] = "Spirit";
-	pat[55] = "Spirit";
-	pat[56] = "Spirit";
-	pat[57] = "Spirit";
-	pat[58] = "Spirit";
-	pat[59] = "Spirit";
-	pat[60] = "DEM";
-	pat[61] = "DEM";
-	pat[62] = "DEM";
-	pat[63] = "DEM";
-	pat[64] = "DEM";
-	pat[65] = "DEM";
-	pat[66] = "DEM";
-	pat[67] = "DEM";
-	pat[68] = "DEM";
-	pat[69] = "DEM";
-	pat[70] = "Angel";
-	pat[71] = "Angel";
-	pat[72] = "Angel";
-	pat[73] = "Angel";
-	pat[74] = "Angel";
-	pat[75] = "Angel";
-	pat[76] = "Angel";
-	pat[77] = "Angel";
-	pat[78] = "Angel";
-	pat[79] = "Angel";
-	pat[80] = "Battle";
-	pat[81] = "Battle";
-	pat[82] = "Battle";
-	pat[83] = "Battle";
-	pat[84] = "Battle";
-	pat[85] = "Battle";
-	pat[86] = "Battle";
-	pat[87] = "Battle";
-	pat[88] = "Battle";
-	pat[89] = "Battle";
-	pat[90] = "Battle";
-	pat[91] = "blade";
-	pat[92] = "blade";
-	pat[93] = "blade";
-	pat[94] = "blade";
-	pat[95] = "blade";
-	pat[96] = "blade";
-	pat[97] = "blade";
-	pat[98] = "blade";
-	pat[99] = "blade";
+	pat[12] = "Origami";
+	pat[13] = "Yoshino";
+	pat[14] = "Shido";
+	pat[15] = "Spirit";
+	pat[16] = "DEM";
+	pat[17] = "Angel";
+	pat[18] = "Battle";
+	pat[19] = "Blade";
 
-	//set up headers
-	my_file << "Character limit " << "," << "Boyer Moore Time taken" << "," << "Rabin Karp Time taken" << endl;
-	for (int i = 0; i < 17; i++)
+	for (int i = 0; i < BOOKLIMIT; i++)
 	{	//load_jute_book(text); //call the load function and pass it the file .txt
 		FileName = "DateALiveVolume" + std::to_string(i + 1) + ".txt";
 		load_file(FileName, text);
-		cout << "String size: " << text.size() << endl;
-		for (int j = 0; j < 100; j++)
+		//cout << "String size: " << text.size() << endl;
+		for (int j = 0; j < (PATTERNCOUNT/2); j++)
 		{
-			//Position pos = find_bruteforce(pat, text);
-			//Position pos = find_skipping(pat, text);
-			//Position pos = find_bm(pat, text);
-			//cout << "Searching for:" << pat << endl;
-			cout << "Boyer Moore" << endl;
+			//cout << "Boyer Moore" << endl;
 
-			//time how long it takes to Search via Boyer Moore
-			the_clock::time_point start = the_clock::now();
 			find_bm_multiple(pat[j], text);
-			the_clock::time_point end = the_clock::now();
-			time_taken[0] = duration_cast<milliseconds>(end - start).count();
-
-			//print the time taken
-			cout << "time taken to Search " << time_taken[0] << "ms" << endl;
-			//	system("pause");
-
-			cout << "Rabin Karp" << endl;
-			// time how long it takes to Search via Rabin karp
-			start = the_clock::now();
+		
 			Rabin_Karp(pat[j], text);
-			end = the_clock::now();
-			time_taken[1] = duration_cast<milliseconds>(end - start).count();
-			//print the time taken
-			cout << "time taken to Search " << time_taken[1] << "ms" << endl << endl;
-			my_file << text.size() << "," << time_taken[0] << "," << time_taken[1] << endl;
 
-			//	cout << endl << endl;
-				//show_context(text, pos);			
 		}
 
-		cout << "book " << i << endl;
+		//cout << "book " << i << endl;
 	}
-	system("pause");
+	//system("pause");
 }
 
 void Threaded_String_search()
 {
-	string pat[20];
+	string pat[PATTERNCOUNT];
 	pat[0] = "244";
 	pat[1] = "Tohka";
 	pat[2] = "Origami";
@@ -448,27 +342,27 @@ void Threaded_String_search()
 
 
 	//THREAD BASED PARALLELEISM CODE
-	thread myThread[20];
+	thread myThread[PATTERNCOUNT];
 	thread printThread;
 	string text;//declare text as a string
 	//float time_taken[2];
 	string FileName = "";
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < BOOKLIMIT; i++)
 	{
 		FileName = "DateALiveVolume" + std::to_string(i+1) + ".txt";
 		load_file(FileName, text);
-		cout << "String size: " << text.size() << endl;
+	//	cout << "String size: " << text.size() << endl;
 
 		printThread = thread(finished_book, i);
 
-		for (int j = 0; j < 10; j++)
+		for (int j = 0; j < (PATTERNCOUNT/2); j++)
 		{
 			//create thread and give it the function to run
 			myThread[j] = thread(find_bm_multiple, pat[j], text);
 			myThread[j + 10] = thread(Rabin_Karp, pat[j + 10], text);
 		}
 		//join the threads
-		for (int j = 0; j < 20; j++)
+		for (int j = 0; j < PATTERNCOUNT; j++)
 		{ //combine all threads
 			myThread[j].join();
 		}
@@ -484,7 +378,27 @@ void Threaded_String_search()
 }
 int main(int argc, char *argv[]) 
 {
-	//old_string_Search();
-	Threaded_String_search();	
+	float time_taken[2];
+	//set up headers for the file
+	my_file << "serial Time taken" << "," << "parallel Time taken" << endl;
+
+	//time how long it takes to Search via serialised searching
+	the_clock::time_point start = the_clock::now();
+	old_string_Search();
+	the_clock::time_point end = the_clock::now();
+	time_taken[0] = duration_cast<milliseconds>(end - start).count();
+
+	//print the time taken
+	cout << "time taken to Search in serial " << time_taken[0] << "ms" << endl;
+
+	//time how long it takes to Search via parallel searching
+	start = the_clock::now();
+	Threaded_String_search();
+	end = the_clock::now();
+	time_taken[1] = duration_cast<milliseconds>(end - start).count();
+	//print the time taken
+	cout << "time taken to Search in parallel" << time_taken[1] << "ms" << endl << endl;
+	//save to our file
+	my_file << time_taken[0] << "," << time_taken[1] << endl;	
 	return 0;
 }
